346e2670c8cdc75bbc83651e26e32c3636e247fe
Sceeker <user@sceeker.com>
2023-01-05 00:23:26 +0100

    TS2531 fix

--- mi_tv_frontend/src/components/ImageModal.vue
+++ mi_tv_frontend/src/components/ImageModal.vue
@@ -106,11 +106,11 @@ export default defineComponent({
       let parts = date.match(/(\d+)/g);
 
       return new Date(
-        parseInt(parts[0]),
-        parseInt(parts[1]) - 1,
-        parseInt(parts[2]),
-        parseInt(parts[3]),
-        parseInt(parts[4])
+        parseInt(parts![0]),
+        parseInt(parts![1]) - 1,
+        parseInt(parts![2]),
+        parseInt(parts![3]),
+        parseInt(parts![4])
       );
     },
   },
