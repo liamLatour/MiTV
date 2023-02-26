30ac477e5dcf0025c7a18b1978d306757badea71
Sceeker <user@sceeker.com>
2023-02-26 18:23:08 +0100

    Fixed URL issues

--- mi_tv_frontend/vite.config.ts
+++ mi_tv_frontend/vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "http://frontend:5173",
   plugins: [vue()],
   resolve: {
     alias: {
