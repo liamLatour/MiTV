4665463347c3a4befedaaab96d578b9c25bb975a
Liam LATOUR <liam.latour@etu.emse.fr>
2023-03-28 13:40:24 +0200

    Sorted images by date

--- mi_tv_frontend/src/http-common.ts
+++ mi_tv_frontend/src/http-common.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const backendURL = "http://backend:5000/";
+export const backendURL = "http://127.0.0.1:5000/"; //"http://backend:5000/";
 
 export default axios.create({
   baseURL: backendURL,
